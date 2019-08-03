65251e40782ead77dab0a282c0fbc71f5716ba0f
Richard Joffray <rjoffray@richards-mbp.lan>
2019-08-02 18:55:00 -0700

    added console logs to work with fluentd demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const oak = require('oak')
 console.log(process.env)
+console.log("REMOTE_URL", process.env.REMOTE_URL)
 oak.on('ready', () => {
   oak.load({
     url: process.env.REMOTE_URL || 'https://www.zivelo.com'
